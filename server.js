190d2f641004bffff56e8527db707463104fce90
yubi00 <ubrajkhadka@gmail.com>
2020-10-06 23:37:11 +1100

    folder struc changed

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express')
 const path = require('path')
 
 const app = express()
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+app.use(express.static(path.join(__dirname, 'client', 'build')))
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'))
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
 })
 
 const port = process.env.PORT || 5000
